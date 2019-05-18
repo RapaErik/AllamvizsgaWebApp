3cd8ac0e465887231d0ede33342a3bcc98b733c6
Rapa Erik <rapa.csek@gmail.com>
2019-05-18 04:17:26 +0300

    wifi light dimmer

--- ControlUnit/Program.cs
+++ ControlUnit/Program.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -16,66 +17,34 @@ namespace ControlUnit
         {
 
             var client = new MqttClient(IPAddress.Parse("127.0.0.1"));//brocker IP addres
+           
 
             client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
             string clientId = Guid.NewGuid().ToString();
             client.Connect(clientId);
 
-            // subscribe to the topic "/home/temperature" with QoS 2
+            
             client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-            /*for (int i = 0; i < 50; i++)
+
+            Thread thr = new Thread(()=>
             {
+                int counter = 0;
+                while (true)
+                {
+                    
+                    Thread.Sleep(500);
+                    client.Publish("/home/heatspeed", Encoding.UTF8.GetBytes((counter % 100).ToString()), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                    Console.WriteLine("/home/heatspeed"+ "      "+ Encoding.UTF8.GetBytes((counter % 100).ToString()));
+                    counter++;
+                }
+            });
+          //  thr.Start();
 
-                temp.TimeStamp = DateTime.Now;
-                string strValue = "5.6";
-                // publish a message on "/home/temperature" topic with QoS 2 
-                client.Publish("/home/temperature", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                System.Threading.Thread.Sleep(500);
-            }*/
-
-
-
-
-
-
-
-
-
-            //WeatherForcast v = new WeatherForcast();
-            //var forcast = v.GetWeatherForcast();
-
-            //SensorData data = new SensorData {  SensorId = 1, /*TimeStamp = DateTime.Now.ToUniversalTime(), */Temperature = 15f, Humidity=33.5f};
-
-            //var jsonSettings = new JsonSerializerSettings();
-            //jsonSettings.DateFormatString = "dd/MM/yyy hh:mm:ss";
-
-            //string json = JsonConvert.SerializeObject(data, jsonSettings);
-            //json = json.ToLower();
-            //Console.WriteLine(json);
-            //Console.WriteLine("Press any key after asp.net is loaded");
-            //Console.ReadKey();
-
-            //var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:62325/api/apisensordata/");
-            //httpWebRequest.ContentType = "application/json";
-            //httpWebRequest.Method = "POST";
-
-            //using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            //{
-            //    streamWriter.Write(json);
-            //    streamWriter.Flush();
-            //    streamWriter.Close();
-            //}
-
-            //var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            //using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            //{
-            //    var result = streamReader.ReadToEnd();
-            //    Console.WriteLine(result);
-            //}
 
 
             Console.ReadKey();
+            
         }
 
         static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
@@ -116,7 +85,8 @@ namespace ControlUnit
                 Console.WriteLine("Nan!!!!");
             }
 
-
         }
+
+
     }
 }
