1348aad6b6418844466668aa4765bd53f2e71877
Rapa Erik <rapa.csek@gmail.com>
2019-05-21 16:39:56 +0300

    Object oriented ControlUnit + hosting application

---
+++ ControlUnit/Communication.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace ControlUnit
+{
+    public class Communication
+    {
+
+        private MqttClient client;
+        
+
+
+        public Communication()
+        {
+            string mqttBorkerIpAddres = "127.0.0.1";
+            client = new MqttClient(mqttBorkerIpAddres);
+
+
+            
+        }
+        public void ConnectMqtt()
+        {
+            client.MqttMsgPublishReceived += MqttController;
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+        }
+
+        public void SubscribeToMqttTopic(string topic)
+        {
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+
+        void IncommingTemperatureData(string msg)
+        {
+            string[] datas = msg.Split(' ');
+            foreach (var item in datas)
+            {
+                Console.WriteLine(item);
+            }
+            var temp = float.Parse(datas[0], CultureInfo.InvariantCulture.NumberFormat);
+            var humi = float.Parse(datas[1], CultureInfo.InvariantCulture.NumberFormat);
+            SensorData data = new SensorData { SensorId = 1, Temperature = temp, Humidity = humi };
+
+            var jsonSettings = new JsonSerializerSettings();
+            jsonSettings.DateFormatString = "dd/MM/yyy hh:mm:ss";
+
+            string json = JsonConvert.SerializeObject(data, jsonSettings);
+            Console.WriteLine(json);
+            HttpWebRequest httpWebRequest;
+            httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:62325/api/apisensordata/");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
+            
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        }
+        void IncommingDataErrorInTopic(string topic)
+        {
+            Console.WriteLine("Nan!!!! at topic:"+topic);
+        }
+        void MqttController(object sender, MqttMsgPublishEventArgs e)
+        {
+            string msg = Encoding.UTF8.GetString(e.Message);
+            if(msg!="NaN")
+            {
+                switch (e.Topic)
+                {
+                    case "/home/temperature":
+                        IncommingTemperatureData(msg);
+                        break;
+
+                    case "/home/humidity":
+                        break;
+
+                    case "/home/heatspead":
+                        break;
+                }
+            }
+            else
+            {
+                IncommingDataErrorInTopic(e.Topic);
+            }
+            
+        }
+
+
+    }
+}
+
--- ControlUnit/Program.cs
+++ ControlUnit/Program.cs
@@ -16,77 +16,33 @@ namespace ControlUnit
         static void Main(string[] args)
         {
 
-            var client = new MqttClient(IPAddress.Parse("127.0.0.1"));//brocker IP addres
-           
+            Communication com = new Communication() ;
 
-            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            com.ConnectMqtt();
+            com.SubscribeToMqttTopic("/home/temperature");
 
-            string clientId = Guid.NewGuid().ToString();
-            client.Connect(clientId);
 
             
-            client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-
-            Thread thr = new Thread(()=>
+            /*
+            Thread thr = new Thread(() =>
             {
                 int counter = 0;
                 while (true)
                 {
-                    
+
                     Thread.Sleep(500);
                     client.Publish("/home/heatspeed", Encoding.UTF8.GetBytes((counter % 100).ToString()), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                    Console.WriteLine("/home/heatspeed"+ "      "+ Encoding.UTF8.GetBytes((counter % 100).ToString()));
+                    Console.WriteLine("/home/heatspeed" + "      " + Encoding.UTF8.GetBytes((counter % 100).ToString()));
                     counter++;
                 }
             });
-          //  thr.Start();
-
+            // thr.Start();
+            */
 
 
             Console.ReadKey();
-            
-        }
-
-        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
-        {
-            
-          
-            if (Encoding.UTF8.GetString(e.Message) != "NaN")
-            {
-                string[] datas = Encoding.UTF8.GetString(e.Message).Split(' ');
-                foreach (var item in datas)
-                {
-                    Console.WriteLine(item);
-                }
-                var temp = float.Parse(datas[0], CultureInfo.InvariantCulture.NumberFormat);
-                var humi = float.Parse(datas[1], CultureInfo.InvariantCulture.NumberFormat);
-                SensorData data = new SensorData { SensorId = 1, Temperature = temp, Humidity = humi };
-
-                var jsonSettings = new JsonSerializerSettings();
-                jsonSettings.DateFormatString = "dd/MM/yyy hh:mm:ss";
-
-                string json = JsonConvert.SerializeObject(data, jsonSettings);
-                Console.WriteLine(json);
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:62325/api/apisensordata/");
-                httpWebRequest.ContentType = "application/json";
-                httpWebRequest.Method = "POST";
-
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                {
-                    streamWriter.Write(json);
-                    streamWriter.Flush();
-                    streamWriter.Close();
-                }
-
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            }
-            else
-            {
-                Console.WriteLine("Nan!!!!");
-            }
 
         }
-
 
     }
 }
